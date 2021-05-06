13896f503b9be7fb391c665ba3817e68a921366f
jav2074 <jav.electrochance@gmail.com>
2021-05-05 23:19:14 -0300

    Add pg.connect

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(buildPath, 'index.html'));
+
+  pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+        if (err) throw err;
+        console.log('Connected to postgres! Getting schemas...');
+        client.query('SELECT table_schema,table_name FROM information_schema.tables;').on('row', function(row) {
+        console.log(JSON.stringify(row));
+        });
+    });
 });
 
 app.listen(PORT);
