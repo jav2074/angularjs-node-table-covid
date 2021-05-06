4c1b9fca6e3d6d211c9b3c0710e3a8b522c27c8c
jav2074 <jav.electrochance@gmail.com>
2021-05-05 23:57:44 -0300

    Add DATABASE_URL & PG

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+// ----------------------------------------------------------------
+console.log('DATABASE_URL: ' + DATABASE_URL);
+var pg = require('pg');
+// ----------------------------------------------------------------
 
 const PORT = process.env.PORT || 5000
 
@@ -10,16 +14,17 @@ app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(buildPath, 'index.html'));
+
+  // ----------------------------------------------------------------
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    var query = client.query('SELECT * FROM your_table');
+    query.on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+  });
+  // ----------------------------------------------------------------
+
 });
 
 app.listen(PORT);
 
-
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM your_table');
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
-});
