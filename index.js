1600168fff5190118125570520e2250b2317b9f8
jav2074 <jav.electrochance@gmail.com>
2021-05-05 23:23:43 -0300

    Add DATABASE_URL

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function(req, res) {
   res.sendFile(path.join(buildPath, 'index.html'));
 
+  console.log('DATABASE_URL: ' + DATABASE_URL);
   pg.defaults.ssl = true;
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-        if (err) throw err;
-        console.log('Connected to postgres! Getting schemas...');
-        client.query('SELECT table_schema,table_name FROM information_schema.tables;').on('row', function(row) {
-        console.log(JSON.stringify(row));
-        });
-    });
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+      if (err) throw err;
+      console.log('Connected to postgres! Getting schemas...');
+      client.query('SELECT table_schema,table_name FROM information_schema.tables;').on('row', function(row) {
+      console.log(JSON.stringify(row));
+      });
+  });
 });
 
 app.listen(PORT);
